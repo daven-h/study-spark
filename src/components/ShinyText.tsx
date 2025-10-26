732081f2f605546f20fc4a300e023a79c5854614
Daven Hill <djbrill03@gmaill.com>
2025-10-26 04:27:46 -0400

    Add TypeScript types to ShinyText component

--- src/components/ShinyText.tsx
+++ src/components/ShinyText.tsx
@@ -1,6 +1,13 @@
 import './ShinyText.css';
 
-const ShinyText = ({ text, disabled = false, speed = 3, className = '' }) => {
+interface ShinyTextProps {
+  text: string;
+  disabled?: boolean;
+  speed?: number;
+  className?: string;
+}
+
+const ShinyText = ({ text, disabled = false, speed = 3, className = '' }: ShinyTextProps) => {
   const animationDuration = `${speed}s`;
 
   return (
